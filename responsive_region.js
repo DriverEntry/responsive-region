76aaad675d7e1ca21bbc4a440096c9a7aa441e44
DriverEntry <@>
2015-03-10 15:24:27 +0300

    initial commit

---
+++ responsive_region.js
@@ -0,0 +1,165 @@
+﻿(function _ResponsiveRegions(Backbone, Marionette, $, _) {
+    'use strict';
+    Marionette.ResponsiveRegion = Marionette.Region.extend({
+        constructor : function(options)
+        {
+            _.extend(this,Backbone.Events);
+            this._templateCache = {};
+            Marionette.Region.prototype.constructor.call(this, options);
+        },
+
+        show : function(view, options)
+        {
+            options = _.defaults(options || {}, {
+                lookupState : true
+            });
+
+            if (this.lookupOptions)
+            {
+                this.undelegateEvents();
+                this.lookupOptions = null;
+            }
+
+            if(options.lookupState)
+            {
+                var lookupOptions = this.lookupOptions = _.isBoolean(options.lookupState) ? {} : options.lookupState;
+
+                _.defaults(lookupOptions, this.defaults, {
+                    entities: this._getEntities(view),
+                    startState : 'sync',
+                    template: '#responsive-region-base-template'
+                });
+                this.lookupOptions.entities = _.isArray(this.lookupOptions.entities) ?
+                    this.lookupOptions.entities : [this.lookupOptions.entities];
+            }
+
+            Marionette.Region.prototype.show.call(this, view, options);
+
+            if(options.lookupState)
+            {
+                this.delegateEvents();
+            }
+        },
+        attachHtml : function(view)
+        {
+            if(!this.lookupOptions)
+            {
+                return Marionette.Region.prototype.attachHtml.call(this, view);
+            }
+
+            var $content;
+            if(this.lookupOptions.template)
+            {
+                var $wrapper = $(
+                    this._templateCache[this.lookupOptions.template] =
+                        this._templateCache[this.lookupOptions.template] ||
+                        $(this.lookupOptions.template).html()
+                );
+                
+                $wrapper.find('.js-model-state-sync').add($wrapper.filter('.js-model-state-sync')).empty().append(view.el);
+                $content = $wrapper;
+            }
+            else
+            {
+                $content = $(view.el);
+            }
+
+            this.$el.empty().append($content);
+
+            switch (this.lookupOptions.startState)
+            {
+                case 'request':
+                    this._handleRequest();
+                    break;
+                case 'error':
+                    this._handleError();
+                    break;
+                default:
+                    this._handleSync();
+            }
+            
+
+        },
+        delegateEvents : function()
+        {
+            _.each(this.lookupOptions.entities, function(entity){
+                this.listenTo(entity, 'request', this._handleRequest);
+                this.listenTo(entity, 'sync', this._handleSync);
+                this.listenTo(entity, 'error', this._handleError);
+            }, this);
+
+
+        },
+        undelegateEvents : function()
+        {
+            var entities = _.isArray(this.lookupOptions.entities) ? this.lookupOptions.entities : [this.lookupOptions.entities];
+            _.each(entities, function(entity){
+                this.stopListening(entity);
+            }, this);
+
+        },
+        handleRequest : function()
+        {
+            this.$el.find('.js-model-state-sync, .js-model-state-error').addClass('hidden');
+            this.$el.find('.js-model-state-request').removeClass('hidden');
+            this.state = 'request';
+        },
+        handleError : function()
+        {
+            this.$el.find('.js-model-state-sync, .js-model-state-request').addClass('hidden');
+            this.$el.find('.js-model-state-error').removeClass('hidden');
+            this.state = 'error';
+        },
+        handleSync : function()
+        {
+            this.$el.find('.js-model-state-error, .js-model-state-request').addClass('hidden');
+            this.$el.find('.js-model-state-sync').removeClass('hidden');
+            this.state = 'sync';
+        },
+        _isBubbled: function (target)
+        {
+            return target && !_.contains(this.lookupOptions.entities, target)
+        },
+        _handleRequest : function(target)
+        {
+            if (this._isBubbled(target)) return;
+            this.handleRequest.apply(this, arguments);
+            this.state = 'request';
+        },
+        _handleError: function (target)
+        {
+            if (this._isBubbled(target)) return;
+            this.handleError();
+            this.state = 'error';
+        },
+        _handleSync: function (target)
+        {
+            if (this._isBubbled(target)) return;
+            this.handleSync();
+            this.state = 'sync';
+        },
+        _getEntities: function(view) {
+            return _.chain(view).pick("model", "collection").toArray().compact().value()
+        }
+    });
+
+    Marionette.ResponsiveOpacityRegion = Marionette.ResponsiveRegion.extend({
+        handleRequest : function()
+        {
+            this.$el.find('.js-model-state-sync, .js-model-state-error').css('opacity', 0.5);
+        },
+        handleError : function()
+        {
+            this.$el.find('.js-model-state-sync').addClass('hidden');
+            this.$el.find('.js-model-state-error').removeClass('hidden').css('opacity', 1);
+        },
+        handleSync : function()
+        {
+            this.$el.find('.js-model-state-error').addClass('hidden');
+            this.$el.find('.js-model-state-sync').removeClass('hidden').css('opacity', 1);
+        },
+        defaults : {
+            template : '#responsive-region-opacity-template'
+        }
+    });
+})(Backbone, Marionette, $, _);
---
+++ tests/responsive_region.test.js
@@ -0,0 +1,156 @@
+﻿QUnit.module('ResponsiveRegionTest');
+var eventSequence = ['request', 'error', 'request', 'sync', 'error', 'request', 'sync' ],
+    styleTest = function(name, regionClass,  expectation)
+{
+    QUnit.test(name, function(assert){
+        expect(1);
+        var model = new Backbone.Collection(),
+            region = new regionClass({el : '#qunit-fixture'}),
+            view = new Marionette.View({collection : model});
+        region.show(view, {
+            lookupState : true
+        });
+        var checkStyling  = function(events, model, region)
+        {
+            var results = [];
+            _.each(events, function(e)
+            {
+                model.trigger(e);
+                results.push(expectation[e].call(region));
+            });
+            return results;
+        };
+        assert.ok(
+            _.every(checkStyling(eventSequence, model, region),
+                function(r){return r}), 'Wrapping elements are properly styled');
+
+    });
+};
+QUnit.test("State tracking test", function (assert) {
+    expect(6);
+
+    var model1 = new Backbone.Collection(),
+        model2 = new Backbone.Collection(),
+        region = new  Marionette.ResponsiveRegion({el : '#qunit-fixture'}),
+        viewWithModel = new Marionette.View({collection : model1}),
+        viewWithoutModel = new Marionette.View();
+
+    region.show(viewWithModel, {
+        lookupState : {
+            startState : 'sync'
+        }
+    });
+
+    assert.equal(region.state, 'sync', 'Match to start state');
+
+    var traceState = function(model, region, eventSequence) {
+            var stateSequence = [];
+            _.each(eventSequence, function(e)
+            {
+                model.trigger(e);
+                stateSequence.push(region.state);
+            });
+            return stateSequence;
+        };
+
+    assert.deepEqual(eventSequence, traceState(model1, region, eventSequence),
+        'Region follows model state with implicit binding');
+
+    region.show(viewWithoutModel, {
+        lookupState : {
+            startState : 'request',
+            entities : model2
+        }});
+
+    assert.equal(region.state, 'request', 'Start state overrides last region state on re-show');
+
+    model1.trigger('sync');
+    assert.equal(region.state, 'request', 'First model is not observed after re-show');
+
+    model2.trigger('error');
+    assert.equal(region.state, 'error', 'Second model is observed with explicit binding');
+
+    region.show(viewWithModel, {
+        lookupState : {
+            startState : 'request',
+            entities : model2
+        }});
+
+    assert.ok(!_.isEqual(eventSequence, traceState(model1, region, eventSequence)) &&
+            _.isEqual(eventSequence, traceState(model2, region, eventSequence)),
+        'Region doesnt use implicit binding when model is provided with options');
+});
+
+styleTest("Base region styling test", Marionette.ResponsiveRegion, {
+    request : function(){
+        return !this.$el.find('.js-model-state-request').hasClass('hidden') &&
+            this.$el.find('.js-model-state-error').hasClass('hidden') &&
+            this.$el.find('.js-model-state-sync').hasClass('hidden');
+    },
+    error : function(){
+        return !this.$el.find('.js-model-state-error').hasClass('hidden') &&
+            this.$el.find('.js-model-state-request').hasClass('hidden') &&
+            this.$el.find('.js-model-state-sync').hasClass('hidden');
+    },
+    sync : function(){
+        return !this.$el.find('.js-model-state-sync').hasClass('hidden') &&
+            this.$el.find('.js-model-state-error').hasClass('hidden') &&
+            this.$el.find('.js-model-state-request').hasClass('hidden');
+    }
+});
+
+styleTest("Opacity region styling test", Marionette.ResponsiveOpacityRegion, {
+    request : function(){
+        var $items = this.$el.find('.js-model-state-sync'),
+            $error = this.$el.find('.js-model-state-error');
+        return $items.css('opacity') == 0.5 &&
+            $error.css('opacity') == 0.5 && (
+            ($items.hasClass('hidden') && !$error.hasClass('hidden')) ||
+            (!$items.hasClass('hidden') && $error.hasClass('hidden'))
+            );
+    },
+    error : function(){
+        return this.$el.find('.js-model-state-error').css('opacity') == 1 &&
+            !this.$el.find('.js-model-state-error').hasClass('hidden')
+            this.$el.find('.js-model-state-sync').hasClass('hidden');
+    },
+    sync : function(){
+        return !this.$el.find('.js-model-state-sync').hasClass('hidden') &&
+            this.$el.find('.js-model-state-sync').css('opacity')  == 1 &&
+            this.$el.find('.js-model-state-error').hasClass('hidden');
+    }
+});
+
+QUnit.test("Ignore bubbled events", function (assert) {
+    var collection = new Backbone.Collection(),
+        model = new Backbone.Model(),
+        region = new  Marionette.ResponsiveRegion({el : '#qunit-fixture'}),
+        view = new Marionette.View({collection : collection});
+    region.show(view, {
+        lookupState : true
+    });
+    collection.add(model);
+    model.trigger('request', model, model.collection);
+    assert.equal(region.state, 'sync', 'region ignores bubbled event');
+    region.show(view, {
+        lookupState : true
+    });
+});
+QUnit.test("Observing multiple models", function (assert) {
+    var collection = new Backbone.Collection(),
+        model = new Backbone.Model(),
+        region = new  Marionette.ResponsiveRegion({el : '#qunit-fixture'}),
+        view = new Marionette.View({collection : collection});
+    region.show(view, {
+        lookupState : {
+            entities : [collection, model]
+        }
+    });
+    collection.add(model);
+    model.trigger('request', model, model.collection);
+    assert.equal(region.state, 'request', 'region observes model');
+
+    collection.trigger('error');
+    assert.equal(region.state, 'error', 'region observes collection');
+});
+
